df8cc68d80e60bf2028c08e5b6380d8252c7dbbf
Ahmed Abasiyanik <76453946+aabasiyanik@users.noreply.github.com>
2023-03-17 18:28:14 -0500

    Update main.js
    api_key edited

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ searchInput.addEventListener("keypress", function (e) {
 
 const getWeather = async (city) => {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[You can put your own API here]`,
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=88bebaeec1e1ad21b64426d502f69e44`,
             { mode: "cors" }
         );
 
@@ -59,7 +59,7 @@ window.addEventListener("load", () => {
             lon = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api_key = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=[You can put your own API here]`
+            const api_key = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=88bebaeec1e1ad21b64426d502f69e44`
 
             fetch(api_key).then((response) => {
                 return response.json();
